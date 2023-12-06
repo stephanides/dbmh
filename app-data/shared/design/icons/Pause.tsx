5b91aafd42f2019f429ebd852ef3507191562c47
Stephanides <stefanmacuga1@gmail.com>
2023-12-06 21:04:58 +0100

    Play pause icons

---
+++ app-data/shared/design/icons/Pause.tsx
@@ -0,0 +1,12 @@
+export const Pause = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path d="M11 7H8V17H11V7Z" fill="#ffffff" />
+    <path d="M13 17H16V7H13V17Z" fill="#ffffff" />
+  </svg>
+);
